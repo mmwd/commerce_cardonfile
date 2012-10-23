413dfe76d22c435c5f885c0f519c24564950fd13
Mike V <recrit@gmail.com>
2012-10-23 16:08:06 -0400

    added a hook for 'commerce_cardonfile_data_can_delete'

--- commerce_cardonfile.api.php
+++ commerce_cardonfile.api.php
@@ -38,3 +38,36 @@ function hook_commerce_cardonfile_data_delete($card_data) {
   // perform custom operations reacting to the deletion of an existing
   // card data
 }
+
+/**
+ * Lets modules prevent the deletion of a particular card data.
+ *
+ * Before a card can be deleted or disabled, other modules are given the chance
+ * to say whether or not the action should be allowed. Modules implementing this
+ * hook can check for reference data or any other reason to prevent a product
+ * from being deleted and return FALSE to prevent the action.
+ *
+ * This is an API level hook, so implementations should not display any messages
+ * to the user (although logging to the watchdog is fine).
+ *
+ * @param $card_data
+ *   The card data to be deleted.
+ *
+ * @return
+ *   TRUE or FALSE indicating whether or not the given card can be deleted.
+ */
+function hook_commerce_cardonfile_data_can_delete($card_data) {
+  if (!empty($card_data['card_id'])) {
+    // Use EntityFieldQuery to look for orders referencing this card and do
+    // not allow the delete to occur if one exists.
+    $query = new EntityFieldQuery();
+
+    $query
+      ->entityCondition('entity_type', 'commerce_order', '=')
+      ->entityCondition('bundle', 'commerce_order', '=')
+      ->fieldCondition('commerce_cardonfile', 'card_id', $card_data['card_id'], '=')
+      ->count();
+
+    return $query->execute() > 0 ? FALSE : TRUE;
+  }
+}
